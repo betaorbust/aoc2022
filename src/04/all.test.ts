b5a53c8e8591670c3bb89ef3543540c44b7ed085
Jacques Favreau <betaorbust@users.noreply.github.com>
2022-12-07 01:08:36 -0800

    feat: Day 4, part 2.

--- src/04/all.test.ts
+++ src/04/all.test.ts
@@ -1,6 +1,6 @@
 import { expect, test, describe } from '@jest/globals';
 import { part1 } from './part1';
-// import { part2 } from './part2';
+import { part2 } from './part2';
 
 const testCasesPt1: [Parameters<typeof part1>[0], ReturnType<typeof part1>][] =
 	[
@@ -15,7 +15,18 @@ const testCasesPt1: [Parameters<typeof part1>[0], ReturnType<typeof part1>][] =
 		],
 	];
 
-// const testCasesPt2: [Parameters<typeof part2>[0], ReturnType<typeof part2>][] = [];
+const testCasesPt2: [Parameters<typeof part2>[0], ReturnType<typeof part2>][] =
+	[
+		[
+			`2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`,
+			4,
+		],
+	];
 
 describe('Day 4', () => {
 	test.each(testCasesPt1)(
@@ -24,10 +35,10 @@ describe('Day 4', () => {
 			expect(part1(input)).toBe(expected);
 		}
 	);
-	//	test.each(testCasesPt2)(
-	//		'Part 2. Input: %s. Output: %s',
-	//		(input, expected) => {
-	//			expect(part2(input)).toBe(expected);
-	//		}
-	//	);
+	test.each(testCasesPt2)(
+		'Part 2. Input: %s. Output: %s',
+		(input, expected) => {
+			expect(part2(input)).toBe(expected);
+		}
+	);
 });
--- src/04/part2.ts
+++ src/04/part2.ts
@@ -1,3 +1,11 @@
-export const part2 = (input: string): string => {
-	return 'not implemented';
-};
+import { parseInput } from './part1';
+
+/**
+ * Check if two ranges overlap
+ */
+function hasOverlap(a: [number, number], b: [number, number]): boolean {
+	return (a[0] <= b[0] && a[1] >= b[0]) || (b[0] <= a[0] && b[1] >= a[0]);
+}
+
+export const part2 = (input: string): number =>
+	parseInput(input).filter(([a, b]) => hasOverlap(a, b)).length;
