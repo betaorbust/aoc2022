80e6535a6336b9ba3f707540d13cf6213f0f7851
Jacques Favreau <betaorbust@users.noreply.github.com>
2022-12-06 23:24:31 -0800

    feat: Day 3, Part 1.

---
+++ src/03/all.test.ts
@@ -0,0 +1,34 @@
+import { expect, test, describe } from '@jest/globals';
+import { part1 } from './part1';
+// import { part2 } from './part2';
+
+const testCasesPt1: [Parameters<typeof part1>[0], ReturnType<typeof part1>][] =
+	[
+		[
+			`vJrwpWtwJgWrhcsFMMfFFhFp
+jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+PmmdzqPrVvPwwTWBwg
+wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+ttgJtRGJQctTZtZT
+CrZsJsPPZsGzwwsLwLmpwMDw`,
+			157,
+		],
+	];
+
+// const testCasesPt2: [Parameters<typeof part2>[0], ReturnType<typeof part2>][] =
+// 	[];
+
+describe('Day 3', () => {
+	test.each(testCasesPt1)(
+		'Part 1. Input: %s. Output: %s',
+		(input, expected) => {
+			expect(part1(input)).toBe(expected);
+		}
+	);
+	// test.each(testCasesPt2)(
+	// 	'Part 2. Input: %s. Output: %s',
+	// 	(input, expected) => {
+	// 		expect(part2(input)).toBe(expected);
+	// 	}
+	// );
+});
